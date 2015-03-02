ea650f0b9eebfe8dd4516ad165a5acc6a4bade60
stefanrvo <stefan@stefanrvo.dk>
2015-03-02 22:08:17 +0100

    Removed comments which wasn't true anymore.

--- Assignment_4/sources/UART.c
+++ Assignment_4/sources/UART.c
@@ -1,31 +1,3 @@
-// UART.c
-// Runs on LM3S811, LM3S1968, LM3S8962, LM4F120
-// Simple device driver for the UART.
-// Daniel Valvano
-// June 17, 2013
-
-/* This example accompanies the books
-  "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
-  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2013
-
-"Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
- 
- Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
-
-// U0Rx (VCP receive) connected to PA0
-// U0Tx (VCP transmit) connected to PA1
-
 #include "../headers/UART.h"
 
 static void uart0_tx_isr(void);
@@ -57,10 +29,7 @@ unsigned char uart0_in_char(void){
 		
   return sys_ringbuf_uchar_pop(buffer_in);
 }
-//------------UART_OutChar------------
-// Output 8-bit to serial port
-// Input: letter is an 8-bit ASCII character to be transferred
-// Output: none
+
 void uart0_send_char(unsigned char data){
 	if(!(UART0_FR_R & UART_FR_TXFF) && sys_ringbuf_uchar_size(buffer_out) == 0)  //check if transmit fifo is full, if not, and buffer is empty, just push to FIFO
 	  UART0_DR_R = data;
