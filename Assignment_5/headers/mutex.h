764c6b872ca9627b38f47cee6b1133771f4c70c8
StefanRvo <stefan@stefanrvo.dk>
2015-03-13 20:51:44 +0100

    Added function try_lock_mutex

--- Assignment_5/headers/mutex.h
+++ Assignment_5/headers/mutex.h
@@ -8,6 +8,13 @@
 
 typedef uint32_t mutex_t;
 
-void lock_mutex(volatile mutex_t* mutex);
+void lock_mutex(volatile mutex_t* mutex); //locks the mutex
+
+//tries to lock the mutex, returns true on success, false on fail
+bool try_lock_mutex(volatile mutex_t* mutex);
+
+//unlock the mutex
 void unlock_mutex(volatile mutex_t* mutex);
+
+//Tests if the mutex is locked.
 bool is_mutex_unlocked(mutex_t *mutex);
